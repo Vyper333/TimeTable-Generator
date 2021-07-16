4e05613dece23225625ff445ed29015a65d90645
Athena Rodrigues <53125219+athena46@users.noreply.github.com>
2021-07-16 23:45:33 +0530

    Add files via upload

---
+++ Time-Table Generator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Time_Table_Generator
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuest_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FacultyDashboard ds = new FacultyDashboard("Guest");
+            ds.Show();
+            this.Hide();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "ABC" && txtPassword.Text == "111")
+            {
+                FacultyDashboard ds = new FacultyDashboard("Admin");
+                ds.Show();
+                this.Hide();
+            }
+        }
+    }
+}
